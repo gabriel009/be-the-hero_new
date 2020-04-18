573230699ed8670837314fcf33e2b5e08bf67a30
Gabriel Nascimento <gabriel.nascimento3110@outlook.com>
2020-04-18 16:51:25 -0300

    Add files via upload

---
+++ build/precache-manifest.02fa3b21c18295e906503c5e7be3f17c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0daa138817a7bd6f93fbb768379c6f26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1153bc2a998ed07e8895",
+    "url": "/static/css/main.64733756.chunk.css"
+  },
+  {
+    "revision": "5fda07c1c0c0986ed2ce",
+    "url": "/static/js/2.456c8e2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.456c8e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1153bc2a998ed07e8895",
+    "url": "/static/js/main.8eb39c67.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "62e865e13ce4dd36023d6f8d6c53bb73",
+    "url": "/static/media/logo.62e865e1.svg"
+  }
+]);
